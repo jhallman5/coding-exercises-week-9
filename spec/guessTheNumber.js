3446fe2f6eaa10650ae480e547a4e721ae7c4bf1
John Hallman <jhallman5@Johns-MacBook-Pro.local>
2017-01-30 17:52:14 -0800

    end of monday worked on guessthe nubmer

---
+++ spec/guessTheNumber.js
@@ -0,0 +1,9 @@
+import guessTheNumber from "../src/guessTheNumber"
+import chai from "chai"
+
+describe("Guess the Number", () => {
+  it("a gives the right alert when the number is correct" , () => {
+
+    expect(alert.) // unclear on how to evalulate an alert
+  })
+})
